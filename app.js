5c6afe77cdcb24a641e748d2bf849bada2b9cd9a
Louis Rawlins <louis.rawlins@gmail.com>
2012-03-04 21:28:55 -0800

    initial rough migration of pages

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var app = module.exports = express.createServer();
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.set('view options', { pretty: true });
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
@@ -30,6 +31,9 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+app.get('/bio', routes.bio);
+app.get('/resume', routes.resume);
+app.get('/contact', routes.contact);
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,17 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: 'Louis Rawlins' })
+};
+
+exports.bio = function(req, res){
+  res.render('bio', { title: 'Louis Rawlins, Bio' })
+};
+
+exports.resume = function(req, res){
+  res.render('resume', { title: 'Louis Rawlins, Resume' })
+};
+
+exports.contact = function(req, res){
+  res.render('contact', { title: 'Louis Rawlins, Contact' })
 };
