d42e1808f305bcf4c99209171ca4f535d2bbb376
Louis Rawlins <louis.rawlins@gmail.com>
2014-06-30 21:30:27 -0700

    clean up for routes and page names, prepping for articles

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,17 @@ exports.index = function(req, res){
 };
 
 exports.resume = function(req, res){
-  res.render('resume', { title: 'Louis Rawlins, Resume' })
+  res.render('resume', { title: 'Resume - Louis Rawlins' })
 };
 
 exports.contact = function(req, res){
-  res.render('contact', { title: 'Louis Rawlins, Contact' })
+  res.render('contact', { title: 'Contact - Louis Rawlins' })
+};
+
+exports.article = function(req, res){
+  res.render('article', { title: 'Articles - Louis Rawlins' })
+};
+
+exports.leanLife = function(req, res){
+  res.render('articles/lean-life', { title: 'Lean Life - Louis Rawlins' })
 };
--- server.js
+++ server.js
@@ -20,6 +20,9 @@ app.configure(function(){
   app.use(express.static(__dirname + '/public'));
 });
 
+// Let's us render HTML to the browser.
+app.register('.html', require('jade'));
+
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
@@ -32,6 +35,10 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 app.get('/contact', routes.contact);
+app.get('/article', routes.article);
+
+// Routes to articles
+app.get('/article/lean-life', routes.leanLife);
 
 // Routes to redirects
 
